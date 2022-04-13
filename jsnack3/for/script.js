39d803ac3b4ec7cd9c0be6742a13afd42a60bd9b
MikeyBadi <mihaibadia.1999@gmail.com>
2022-04-13 16:54:55 +0200

    exercise 3 with for cycle

---
+++ jsnack3/for/script.js
@@ -0,0 +1,8 @@
+total = 0
+
+for(let i = 0; i < 5; i++ ){
+  let number = parseInt(prompt(`Inserisci il ${i} numero`));
+  total += number;
+};
+
+const result = document.querySelector('#result').innerHTML = `La somma totale è: ${total}`
