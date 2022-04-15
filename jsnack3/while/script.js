ce5f61d7857f0c892e0c0cab4c39a72498560a7f
MikeyBadi <mihaibadia.1999@gmail.com>
2022-04-15 22:46:20 +0200

    While cycle added to ex 3

---
+++ jsnack3/while/script.js
@@ -0,0 +1,14 @@
+let total = 0;
+const richieste = 5;
+
+c = 0
+
+while(richieste > c){
+  let number = parseInt(prompt(`Inserisci il ${c} numero`));
+
+  total += number;
+
+  c++
+}
+
+const result = document.querySelector('#result').innerHTML = `La somma totale è: ${total}`;
