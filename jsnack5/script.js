66759521de1ac2a12a2a042694a8bd160548ebfe
MikeyBadi <mihaibadia.1999@gmail.com>
2022-04-15 22:46:39 +0200

    Now is writing even the even numbers not only odd

--- jsnack5/script.js
+++ jsnack5/script.js
@@ -1,4 +1,5 @@
 const arrOdd = []
+const arrEven = []
 
 for(let i = 0; i < 6; i++){
   const numero = parseInt(prompt("Inserisci il numero intero"))
@@ -7,9 +8,13 @@ for(let i = 0; i < 6; i++){
 
   if(numero % 2){
     arrOdd.push(numero)
+  } else {
+    arrEven.push(numero)
   }
 
   console.log(arrOdd);
 };
 
-document.querySelector("#result").innerHTML = `I numeri dispari che hai inserito sono = ${arrOdd}`
+document.querySelector("#resultOdd").innerHTML = `I numeri dispari che hai inserito sono = ${arrOdd}`
+
+document.querySelector("#resultEven").innerHTML = `gli altri numeri che hai inserito sono pari = ${arrEven}`
